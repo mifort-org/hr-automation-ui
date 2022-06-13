d4b56b2d0d4cad94969ec9be8a7276b360223b00
Alex Susha <a.susha@digiteum.pl>
2022-06-13 09:33:39 +0300

    fix: Fixed merge conflict and did eslint --fix

--- src/app/utils/pipes/fullname.pipe.ts
+++ src/app/utils/pipes/fullname.pipe.ts
@@ -5,8 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FullnamePipe implements PipeTransform {
   transform(attribute: any): string {
-    if (attribute?.firstname?.value && attribute?.lastname?.value)
+    if (attribute?.firstname?.value && attribute?.lastname?.value) {
       return `${attribute?.firstname?.value} ${attribute?.lastname?.value}`;
+    }
     return 'No name';
   }
 }
--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -6,13 +6,16 @@ const EMPTY_VALUES = ['', undefined, null];
 
 export const VALIDATORS = {
   identifiedOfField(identifiedArray: AttributeType[], form: FormGroup): ValidationErrors | null {
-    if (identifiedArray.some((el) => !EMPTY_VALUES.includes(form.controls[el?.name]?.value)))
+    if (identifiedArray.some((el) => !EMPTY_VALUES.includes(form.controls[el?.name]?.value))) {
       return null;
+    }
     return { identifiedOfField: TEXT_FIELD_ERRORS.IDENTIFIER_FIELD };
   },
 
   identifiedOfData(identifiedArray: AttributeType[], data: any): string | null {
-    if (identifiedArray.some((el) => !EMPTY_VALUES.includes(data[el?.name]))) return null;
+    if (identifiedArray.some((el) => !EMPTY_VALUES.includes(data[el?.name]))) {
+      return null;
+    }
     return TEXT_FIELD_ERRORS.IDENTIFIER_FIELD;
   },
 };
