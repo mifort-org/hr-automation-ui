12399da18ea2ad572df7978c68dd9f03069e4d22
nanamiquchadze <nanamiquchadze@mifort.org>
2022-09-14 18:41:47 +0400

    feat: STATUS_COLOR constant added

--- src/app/constants/candidates.ts
+++ src/app/constants/candidates.ts
@@ -18,3 +18,13 @@ export const CANDIDATE_STATUSES = [
     value: CandidateStatus.CREATED,
   },
 ];
+
+export interface Color {
+  [key: string]: string;
+}
+
+export const STATUS_COLOR: Color = {
+  [CandidateStatus.UNEMPLOYED]: 'rgba(244, 67, 54, 1)',
+  [CandidateStatus.EMPLOYED]: 'rgba(40, 194, 101, 1)',
+  [CandidateStatus.CREATED]: 'rgba(0, 137, 255, 1)',
+};
