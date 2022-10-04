ee4ce9bb5bedb8c440a6dade6e127e5e1435fa37
nanamiquchadze <nana.miquchadze@mifort.org>
2022-10-04 14:43:32 +0400

    fix: modify return values

--- src/app/pages/candidates/candidate-item/candidate-item.component.ts
+++ src/app/pages/candidates/candidate-item/candidate-item.component.ts
@@ -9,26 +9,29 @@ import { Candidate } from '@src/app/models/candidate';
 export class CandidateItemComponent implements OnInit {
   @Input() candidate: Candidate | null = null;
 
-  contacts: boolean = false;
+  hasContacts: boolean = false;
 
-  location: boolean = false;
+  hasLocation: boolean = false;
 
   ngOnInit(): void {
-    this.contacts = this.checkContacts();
-    this.location = this.checkLocation();
+    this.hasContacts = this.checkContacts();
+    this.hasLocation = this.checkLocation();
   }
 
   checkContacts(): boolean {
-    if (!this.candidate?.contacts?.email && !this.candidate?.contacts?.phone) {
-      return false;
-    }
-    return true;
+    // if (!this.candidate?.contacts?.email && !this.candidate?.contacts?.phone) {
+    //   return false;
+    // }
+    // return true;
+    return Boolean(this.candidate?.contacts?.email || this.candidate?.contacts?.phone);
   }
 
   checkLocation(): boolean {
-    if (!this.candidate?.city) {
-      return false;
-    }
-    return true;
+    //   if (!this.candidate?.city) {
+    //     return false;
+    //   }
+    //   return true;
+    // }
+    return Boolean(this.candidate?.city);
   }
 }
