b586f8fef060826f3c3f5829dcfb7941c04c2917
Rati Abulashvili <ratiabula4hvili@gmail.com>
2022-10-06 01:07:36 +0400

    UI: update candidates attributes list

--- src/app/pages/candidate-detail/candidate-main-info/candidate-main-info.component.ts
+++ src/app/pages/candidate-detail/candidate-main-info/candidate-main-info.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { CandidateDetailService } from '@services/candidate-detail.service';
 import { Candidate } from '@src/app/models/candidate';
+import { ModalService } from '@src/app/services/modal.service';
+import { EditCandidateModalComponent } from '@pages/candidate-detail/edit-candidate-modal/edit-candidate-modal.component';
+import { EModalSizes } from '@constants/strings';
 
 @Component({
   selector: 'app-candidate-main-info',
@@ -9,13 +11,42 @@ import { Candidate } from '@src/app/models/candidate';
   templateUrl: './candidate-main-info.component.html',
 })
 export class CandidateMainInfoComponent implements OnInit {
-  public candidate$!: Observable<Candidate>;
+  public candidate!: Candidate;
+
+  public contactInformation: { name: string; value: string | undefined; icon: string }[] = [];
 
   public displayedColumns: string[] = ['name', 'value'];
 
-  constructor(private candidateDetailService: CandidateDetailService) {}
+  constructor(
+    private candidateDetailService: CandidateDetailService,
+    private modalService: ModalService
+  ) {}
 
   ngOnInit(): void {
-    this.candidate$ = this.candidateDetailService.currentCandidate$;
+    this.candidateDetailService.currentCandidate$.subscribe((candidate: Candidate) => {
+      this.candidate = candidate;
+      this.modifyContactInfo();
+    });
+  }
+
+  // modify contact info array for use with angular material and add icons
+  public modifyContactInfo() {
+    Object.keys(this.candidate.contacts!).forEach((key) => {
+      let icon = '';
+      this.candidate.candidateAttributes.forEach((attributes) => {
+        if (attributes.attributeTypes.name === key) {
+          icon = attributes.attributeTypes.icon!;
+        }
+      });
+      this.contactInformation.push({
+        name: key,
+        value: this.candidate.contacts![key as keyof typeof this.candidate.contacts],
+        icon,
+      });
+    });
+  }
+
+  public openEditModal(): void {
+    this.modalService.open(EditCandidateModalComponent, EModalSizes.MD);
   }
 }
