5e762a838bad7e5e4dd4181e24fad06b3e2c0649
Sophie Jioshvili <sophie.jioshvili@mifort.org>
2022-10-13 11:31:22 +0400

    fix

--- src/app/pages/candidate-detail/candidate-detail.component.ts
+++ src/app/pages/candidate-detail/candidate-detail.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CandidateDetailService } from '@services/candidate-detail.service';
 import { Candidate } from '@src/app/models/candidate';
+import { AddCommentModalComponent } from '@pages/candidate-detail/add-comment-modal/add-comment-modal.component';
+import { DialogModalIds, EModalSizes } from '@constants/strings';
+import { ModalService } from '@services/modal.service';
 
 @Component({
   selector: 'app-candidate-detail',
@@ -14,13 +17,26 @@ export class CandidateDetailComponent implements OnInit {
 
   public currentCandidate!: Candidate;
 
-  constructor(private candidateDetailService: CandidateDetailService) {}
+  constructor(
+    private candidateDetailService: CandidateDetailService,
+    private modalService: ModalService
+  ) {}
 
   ngOnInit(): void {
     this.candidate$ = this.candidateDetailService.currentCandidate$;
     this.candidateDetailService.currentCandidate$.subscribe((res: Candidate) => {
       this.currentCandidate = res;
-      console.log('keywords', res.keywords);
+      console.log('keywords', this.currentCandidate, res.keywords);
     });
+  }
+
+  public openAddCommentModal(): void {
+    this.modalService.open(
+      AddCommentModalComponent,
+      EModalSizes.MD,
+      null,
+      undefined,
+      DialogModalIds.addCommentModal
+    );
   }
 }
